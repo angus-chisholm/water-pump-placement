1ad60fae71200432ee87c0510abeb0a82edd8af4
Angus Chisholm <ac2518@cam.ac.uk>
2025-03-11 18:21:31 +0100

    2 models working

--- water-optimization-project/src/optimisation_pymoo_distance.py
+++ water-optimization-project/src/optimisation_pymoo_distance.py
@@ -22,22 +22,26 @@ pos_pumps = pumps[['Lon','Lat']].to_numpy() # For f2 (min distance)
 cost_borehole=5000
 cost_standpipe=500
 cost_per_meter=2
+max_distance = 800
 
-def impact(pump_positions, household_positions, household_capita, x = None):
+def impact(pump_positions, household_positions, household_capita, max_dist, x = None):
     pump_positions_copy = pump_positions.copy()
     if isinstance(x, np.ndarray):
         pump_positions_copy = np.concatenate((pump_positions_copy,np.array([x])),axis = 0)
-    min_pump_distance = np.zeros(len(household_positions))
+    within_max_distance = np.zeros(len(household_positions))
     for index,pos_houseshold in enumerate(household_positions):
         dist = np.zeros(len(pump_positions_copy))
         for i,pump_pos in enumerate(pump_positions_copy):
             dist[i] = geo.great_circle(pump_pos, household_positions[index]).meters
-        min_pump_distance[index] = np.min(dist)
+        if np.min(dist) <= max_dist:
+            within_max_distance[index] = 1
+        else:
+            within_max_distance[index] = 0
 
-    impact = np.sum(household_capita * (min_pump_distance))
-    return impact
+    impact = np.sum(household_capita * (within_max_distance))
+    return -1*impact
 
-initial_impact = impact(pos_pumps,pos_households,nb_capita)
+initial_impact = impact(pos_pumps,pos_households,nb_capita,max_distance)
 
 
 bounds = np.array([
@@ -52,7 +56,7 @@ class MyProblem(ElementwiseProblem):
 
     def _evaluate(self, x, out, *args, **kwargs):
         # Compute weighted sum of distances to points in pos_households
-        f1 = impact(pos_pumps,pos_households,nb_capita,x)-initial_impact  # Negative impact calculation with new x location
+        f1 = impact(pos_pumps,pos_households,nb_capita,max_distance,x)-initial_impact  # Negative impact calculation with new x location
         
         # Compute minimum distance to any point in pos_pumps
         f2_distances = np.zeros(len(pos_pumps))
@@ -60,9 +64,9 @@ class MyProblem(ElementwiseProblem):
             f2_distances[index] = geo.great_circle(pump, x).meters
         f2 = cost_borehole+cost_standpipe+cost_per_meter*np.min(f2_distances)  # Minimum distance to any point in pos_pumps
 
-        # Define constraint: f1 >= c ## could change to be above initial impact!!
-        c = initial_impact - 0.5
-        g = f1-c
+        # Define constraint: f1 <= c ## could change to be above initial impact!!
+        c = 0
+        g = f1-c #<=0
 
         out["F"] = [f1, f2]  # Objective functions
         out["G"] = [g]  # Constraints (must be <= 0)
@@ -75,7 +79,7 @@ def optimise_nsgaII():
         pop_size=100,
         sampling=FloatRandomSampling(),
         crossover=SBX(prob=0.9, eta=15),
-        mutation=PM(prob=0.1, eta=20),
+        mutation=PM(prob=0.5, eta=20),
         eliminate_duplicates=True
     )
 
@@ -89,16 +93,20 @@ def optimise_nsgaII():
                 verbose=True)
 
     # Plot the Pareto front (objective values)
-    Scatter().add(res.F).show()
+    plot = Scatter()
+    plot.add(res.F)
+    plot.show()
     #best_index = np.argmin(res.F)
-    return res.X,res.F
+    return res.X,res.F,res.G
 
-sol_pos,sol_val = optimise_nsgaII()
+sol_pos,sol_val,sol_constr = optimise_nsgaII()
 
 
 pos_pumps = np.concatenate((pos_pumps,sol_pos), axis = 0)
 
 plt.scatter(households['Lon'].to_numpy(), households['Lat'].to_numpy(), c='blue', label='Household')
 plt.scatter(pos_pumps[:,0],pos_pumps[:,1],c='red',label='pump')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
 plt.legend()
 plt.show()
